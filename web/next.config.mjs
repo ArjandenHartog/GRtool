8466d3d74642f244a8826947fe1522a5ec7e3349
ArjandH <arjandenh@outlook.com>
2026-03-06 18:34:32 +0100

    feat: Add election data for 2014, 2018, and 2022, along with a data build script and XML parsing utility.

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    return config;
-  },
-  turbopack: {},
-  outputFileTracingIncludes: {
-    '/*': ['./data/2022/*.xml', './data/2018/*.xml', './data/2014/*.xml'],
-    '/': ['./data/2022/*.xml', './data/2018/*.xml', './data/2014/*.xml']
-  }
 };
 
 export default nextConfig;
