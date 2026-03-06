e884ba5b7640285a7d3a0df60285ebb2a9d446cd
ArjandH <arjandenh@outlook.com>
2026-03-06 16:24:27 +0100

    Update TypeScript references and configure output file tracing exclusions in Next.js

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  outputFileTracingExcludes: {
+    '/': ['../2022/Telling_*', '../2018/Telling_*', '../2014/Telling_*'],
+  },
 };
 
 export default nextConfig;
